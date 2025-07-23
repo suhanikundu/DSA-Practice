328a11de612b3730a05ffaa568521430ff50e6a4
Suhani Kundu <126338201+suhanikundu@users.noreply.github.com>
2025-07-23 13:11:45 +0530

    Time: 0 ms (100%), Space: 19.6 MB (87.38%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,25 @@
+void moveZeroes(int* nums, int numsSize) {
+    
+    int j=-1;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]==0)
+        {
+            j=i;
+            break;
+        }
+    }
+
+    if(j == -1) return;
+
+    for(int i=j+1;i<numsSize;i++)
+    {
+        if(nums[i]!=0)
+        {
+            int temp= nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            j++;
+        }
+    }
+}
